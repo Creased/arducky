a5a76ec056471803fd1fc7fae40d394c96145a8e
Baptiste MOINE <bap.moine.86@gmail.com>
2017-08-18 13:51:59 +0200

    Add SD card LED blinking warning

--- arducky.ino
+++ arducky.ino
@@ -38,6 +38,12 @@ void setup() {
 
     // Initialize SD Card
     if (!SD.begin(4)) {
+        for (int i=0; i <= 25; i++) {
+            digitalWrite(LED, HIGH);   // turn the LED on (HIGH is the voltage level)
+            delay(100);                // wait for a second
+            digitalWrite(LED, LOW);    // turn the LED off by making the voltage LOW
+            delay(100);                // wait for a second
+        }
         return;
     }
 
